08cb6c162136075a25e8b5b2ed3061bb8aab18cc
vhperaltaj <victor.peralta@microside.com>
2023-07-11 09:44:09 -0600

    Initial commit

---
+++ Proyecto/PIC18F4550/19_SR04.X/LCD.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   LCD.h
+ * Author: Enrique
+ *
+ * Created on 25 de noviembre de 2020, 01:51 PM
+ */
+
+#ifndef LCD_H
+#define	LCD_H
+
+#define RS LATB0                    /*PIN 0 of PORTB is assigned for register select Pin of LCD*/
+#define EN LATB1                    /*PIN 1 of PORTB is assigned for enable Pin of LCD */
+#define ldata LATB                  /*PORTB(PB4-PB7) is assigned for LCD Data Output*/ 
+#define LCD_Port TRISB              /*define macros for PORTB Direction Register*/
+
+#define _XTAL_FREQ 48000000
+
+void LCD_Init(void); /*Initialize LCD*/
+void LCD_Command(unsigned char); /*Send command to LCD*/
+void LCD_Char(unsigned char x); /*Send data to LCD*/
+void LCD_String(const char *); /*Display data string on LCD*/
+void LCD_String_xy(char, char, const char *);
+void LCD_Clear(void); /*Clear LCD Screen*/
+
+
+#endif	/* LCD_H */
+
