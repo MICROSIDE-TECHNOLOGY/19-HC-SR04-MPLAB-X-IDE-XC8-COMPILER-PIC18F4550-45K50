40f3ed2881d0dfa87c7a3126f19db34db148938a
vhperaltaj <victor.peralta@microside.com>
2023-09-12 14:45:00 -0600

    Added comments and updated indentation

--- Proyecto/PIC18F45K50/19_SR04.X/main.c
+++ Proyecto/PIC18F45K50/19_SR04.X/main.c
@@ -19,79 +19,65 @@
  */
 
 /*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
+    (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip software and any
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party
+    license terms applicable to your use of third party software (including open source software) that
     may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
     FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
     SOFTWARE.
  */
 
+#include "lcd.h"
 #include "mcc_generated_files/mcc.h"
 #include <stdio.h>
-#include "lcd.h"
-
 
 int distancia;
 
 char SDistancia[20];
 
-int GetDistance(void);
-void main(void) {
-    // Initialize the device
-    SYSTEM_Initialize();
+int GetDistance( void );
+void main( void )
+{
+    SYSTEM_Initialize();                // Configura el dispositivo
     Lcd_Init();
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
-    // Use the following macros to:
 
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
-
-    // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
-
-    while (1) {
-        distancia=GetDistance();
-        sprintf(SDistancia, "%d", distancia);
+    while ( 1 ) {
+        distancia = GetDistance();      // Calcula la distancia en cm
+        sprintf( SDistancia, "%d", distancia );
         Lcd_Clear();
-        Lcd_Set_Cursor(1, 1);
-        Lcd_Write_String("Distancia:");
-        Lcd_Set_Cursor(2, 1);
-        Lcd_Write_String(SDistancia);
-        __delay_ms(1000);
+        Lcd_Set_Cursor( 1, 1 );
+        Lcd_Write_String( "Distancia:" );
+        Lcd_Set_Cursor( 2, 1 );
+        Lcd_Write_String( SDistancia ); // Muestra la distancia en la pantalla LCD
+        __delay_ms( 1000 );
     }
 }
 
-int GetDistance(void){
-    int cm=0;
+int GetDistance( void )
+{
+    int cm = 0;
     TRIG_SetHigh();
-    __delay_us(10);
+    __delay_us( 10 );
     TRIG_SetLow();
-    while(ECHO_GetValue()==0);
-    while(ECHO_GetValue()==1){
+    while ( ECHO_GetValue() == 0 )
+        ;
+    while ( ECHO_GetValue() == 1 ) {
         cm++;
-        __delay_us(58);
+        __delay_us( 58 );
     }
-    return (cm);
+    return ( cm );
 }
